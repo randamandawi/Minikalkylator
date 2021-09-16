06d3f78d97957ed0cab65e5a18e59e4999833046
Randamandawi <70172778+Randamandawi@users.noreply.github.com>
2021-09-16 11:56:03 +0200

    Add project files.

---
+++ Minikalkylator/Function1.cs
@@ -0,0 +1,42 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Minikalkylator
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+            int input1 = 0;
+            Int32.TryParse(name, out input1);
+
+            string name2 = req.Query["name"];
+            int input2 = 0;
+            Int32.TryParse(name, out input2);
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            var sum = input1 + input2;
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {sum}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(sum);
+        }
+    }
+}
